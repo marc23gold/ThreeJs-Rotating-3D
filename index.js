b552f673e60368f15a11aac07d8dd66819d3a443
Marcx <haitianhero6@gmail.com>
2024-08-26 00:32:29 -0400

    rotating Icosahedron created

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
-console.log('Robot Bobby was here');
+import * as THREE from "three";
+
+// Set the size of the window
+const w = window.innerWidth;
+const h = window.innerHeight;
+
+// Create renderer 
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+
+// Set renderer width and height
+renderer.setSize(w, h);
+// Append to the DOM the canvas element  
+document.body.appendChild(renderer.domElement);
+
+// Set up the camera
+const fov = 75;
+const aspect = w / h;
+const near = 0.1;
+const far = 10;
+const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+camera.position.z = 2;
+
+// Create a scene
+const scene = new THREE.Scene();
+
+const geo = new THREE.IcosahedronGeometry(1.0, 2);
+const mat = new THREE.MeshStandardMaterial({ color: 0xffffff, flatShading: true });
+const mesh = new THREE.Mesh(geo, mat);
+scene.add(mesh);
+
+//add light to the scene
+const hemiLight = new THREE.HemisphereLight(0xffffbb, 0x080820);
+scene.add(hemiLight);
+
+const wireMat = new THREE.MeshBasicMaterial({ color: 0xffffff, wireframe: true });
+const wireMesh = new THREE.Mesh(geo, wireMat);
+scene.add(wireMesh);
+mesh.add(wireMesh);
+
+
+function animate(t=0) {
+    requestAnimationFrame(animate);
+    mesh.rotation.y = t * 0.0001;
+    renderer.render(scene, camera);
+}
+
+animate();
